bbea49d9dbd3bcdbc300a883d94b5a89df39f83f
Chris Martin <chris.bizunit34@gmail.com>
2019-08-21 10:15:28 -0600

    corrected the API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
-    API_ENDPOINT: `http://localhost:8000/api`,
+    API_ENDPOINT: `https://nameless-dusk-62231.herokuapp.com/api`,
     TOKEN_KEY: 'mobile-bookshelf-auth-token',
     USER_NAME: 'mobile-bookshelf-user-name'
   }
-//https://nameless-dusk-62231.herokuapp.com
